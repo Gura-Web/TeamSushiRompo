6d280ec83864747c3808d9cf50f59135f83404a8
三木 百花 <miki.momoka.0317@gmail.com>
2019-08-15 18:25:58 +0900

    メニュー,生活チェックボタン 押した時の画面移動 完了

--- app/js/event.js
+++ app/js/event.js
@@ -1,16 +1,12 @@
 $(function(){
-  let scTop = $(".sc-top");
   let scRegi = $(".sc-regi");
   let scLog = $(".sc-log");
   let scConf = $(".sc-conf");
   let scComp = $(".sc-comp");
-
   let btnMoveRegi = $(".btn-move-regi");
   let btnMoveLog = $(".btn-move-log");
   let btnReturn = $(".btn-return");
   
-  
-
   // 新規登録画面へ移動
   btnMoveRegi.on("click",function(){
     scRegi.addClass("show");
@@ -40,6 +36,85 @@ $(function(){
   // 前の画面へ移動
   btnReturn.on("click",function(){
     $(this).parents(".sc").removeClass("show");
+  })
+
+  // ログイン後 メニュー押した時の動き
+  let btnMoveHome = $(".btn-move-home");
+  let btnMoveMy = $(".btn-move-my");
+  let btnMoveSet = $(".btn-move-set");
+  // ホーム画面
+  let scCheck = $(".sc-check");
+  let scQuest = $(".sc-quest");
+  let btnCheck = $(".btn-check");
+  let btnNext = $(".btn-next");
+  let btnResult = $(".btn-result");
+  let result = $(".result");
+  let afNav = $(".af-nav");
+
+  // 上の3つ囲んだタグ
+  let afScreens = $(".af-screens");
+
+  btnMoveHome.on("click",function(){
+    afScreens.css("left","100%");
+    // 生活チェックから帰ってきた時だけでいい↓
+    if(result.hasClass("on")){
+      setTimeout(function () {
+        scCheck.removeClass("on");
+        scQuest.removeClass("on");
+        result.removeClass("on");
+        setTimeout(function(){
+          scCheck.css("z-index", 10);
+        },400)
+      }, 400)
+    }
+  })
+  btnMoveMy.on("click", function () {
+    afScreens.css("left", "0%");
+    if (result.hasClass("on")) {
+      setTimeout(function () {
+        scCheck.removeClass("on");
+        scQuest.removeClass("on");
+        result.removeClass("on");
+        setTimeout(function () {
+          scCheck.css("z-index", 10);
+        }, 400)
+      }, 400)
+    }
+  })
+  btnMoveSet.on("click", function () {
+    afScreens.css("left", "-100%");
+    if (result.hasClass("on")) {
+      setTimeout(function () {
+        scCheck.removeClass("on");
+        scQuest.removeClass("on");
+        result.removeClass("on");
+        setTimeout(function () {
+          scCheck.css("z-index", 10);
+        }, 400)
+      }, 400)
+    }
+  })
+
+  // ホーム画面
+  btnCheck.on("click",function(){
+    scCheck.addClass("on");
+    setTimeout(function(){
+      afNav.addClass("down");
+    },400)
+  })
+
+  btnNext.on("click",function(){
+    let nowNum = $(this).parents(".sc-quest").attr("class").split(" ")[1].split("0")[1];
+    let nextNum = Number(nowNum) + 1;
+    $(".quest0" + nextNum).addClass("on");
+  })
+
+  btnResult.on("click",function(){
+    result.addClass("on");
+    scCheck.css("z-index",1);
+    setTimeout(function(){
+      afNav.removeClass("down");
+    },100)
   })
 
 
