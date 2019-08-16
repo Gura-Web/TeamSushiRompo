2ff14a7f13d25ab6fb57b9b854478331acff2633
Yanyan Liu <amoryanyan@gmail.com>
2019-08-16 18:51:52 +0900

    新規登録部分のDB完成

--- app/api_regist.php
+++ app/api_regist.php
@@ -9,6 +9,54 @@
 
 
   // 登録するだけのPHP
+  require_once __DIR__ . "/define.php";
+  $name = $_GET["name"];
+  // print $name;
+  $mail=$_GET["mail"];
+  $password=$_GET["pass"];
+  $sex=$_GET["gender"];
+  $birth=$_GET["birth"];
+  $partner=$_GET["partner"];
+  $hashSalt = hash(HASH_ALG, HASH_SALT);
+  $hashPassword = hash(HASH_ALG, $password . $hashSalt);
+  if ($instance = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME)) {
+    $instance->set_charset("utf8");
+    $sql = "SELECT * FROM hack_u_user WHERE mail='{$mail}'";
+    if (!$r = $instance->query($sql)) {
+      print $sql;
+      exit;
+    }
+    if (!$r->num_rows) {
+      $sql = "INSERT INTO hack_u_user(
+        user_name,
+        password, 
+        mail, 
+        sex,
+        bir,
+        partner,
+        created_at
+    )VALUES(
+        '{$name}',
+        '{$hashPassword}',
+        '{$mail}',
+        {$sex},
+        '{$birth}',
+        {$partner},
+        now()
+    )";
+    }
+    if (!$instance->query($sql)) {
+      print $sql;
+    }
+    $instance->close();
+  }
+  else {
+    // print "データベースの接続に失敗しました";
+    header("Location:index.html");
+    exit;
+  }
+
+  
   
 
   // 登録完了できたら "true" を返す
---
+++ app/define.php
@@ -0,0 +1,56 @@
+<?php
+//
+// define.php
+// 定数を定義するファイル
+
+//kenkenlocal
+define( "DB_HOST", "localhost" );
+define( "DB_USER", "root" );
+define( "DB_PASS", "699177" );
+define( "DB_NAME", "yanyan" );
+
+//kenkenclick
+// define( "DB_HOST", "localhost" );
+// define( "DB_USER", "yanyan" );
+// define( "DB_PASS", "eccMyAdmin" );
+// define( "DB_NAME", "yanyan" );
+
+// miki local
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "yururinpanda");
+// define("DB_NAME", "kanpai");
+
+// miki 仮サーバー
+// define("DB_HOST", "mysql1019.db.sakura.ne.jp");
+// define("DB_USER", "mmiki-web");
+// define("DB_PASS", "yururinpanda0317");
+// define("DB_NAME", "mmiki-web_kanpai");
+
+
+// click
+// define( "DB_HOST", "localhost" );
+// define( "DB_USER", "wot" );
+// define( "DB_PASS", "phpMyAdmin" );
+// define( "DB_NAME", "wot" );
+// コメント切り替えで便利
+
+//
+// テーブル関係
+//
+
+// 認証テーブル
+// define( "TBL_AUTH","php3_auth" );
+// ユーザーテーブル
+// define( "TBL_USER","php3_user" );
+
+//
+// パスワード関係
+//
+
+// ハッシュアルゴリズム
+define( "HASH_ALG","sha256" );
+// パスワードソルト
+define( "HASH_SALT","PHP3" );
+
+?>
